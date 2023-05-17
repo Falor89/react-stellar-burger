527b861b7a3dd3c9eb4fdc0e955dbcbe6a51f93c
Maxim Bobyr <max89.bobyr@yandex.ru>
2023-05-18 00:36:14 +0300

    feat: Добавил шапку

---
+++ src/components/appHeader/AppHeader.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Component } from "react";
+import appHeaderStyles from './appHeader.module.css';
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+
+
+export default class AppHeader extends Component {
+    render() {
+        return (
+            <header className={`${appHeaderStyles.header}`}>
+                <nav className={`${appHeaderStyles.navigation}`}>
+                    <ul className={`${appHeaderStyles.menu}`}>
+                        <li className={`${appHeaderStyles.menuItem}`}>
+                            <BurgerIcon type='primary' className={appHeaderStyles.icon} />
+                            <span className={`text text_type_main-default text_color_inactive`}>Конструктор</span>
+                        </li>
+                        <li className={`${appHeaderStyles.menuItem}`}>
+                            <ListIcon type='secondary' className={appHeaderStyles.icon} />
+                            <span className={`text text_type_main-default text_color_inactive`}>Лист заказов</span>
+                        </li>
+                    </ul>
+                    <Logo />
+                    <ul className={`${appHeaderStyles.menu}`}>
+                        <li className={`${appHeaderStyles.profile}`}>
+                            <ProfileIcon type='secondary' className={`${appHeaderStyles.icon}`} />
+                            <span className={`text text_type_main-default text_color_inactive`}>Личный кабинет</span>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
+}
