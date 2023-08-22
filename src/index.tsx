dc9777731bdac5f8e68ad998faa91a46d3291dc9
Maxim Bobyr <max89.bobyr@yandex.ru>
2023-08-22 17:35:01 +0300

    fix: component app

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import App from './components/app/app';
+import App from './components/App/app';
 import { compose, applyMiddleware } from 'redux';
 import { Provider } from "react-redux";
 import thunk from 'redux-thunk';
 import { rootReducer } from './services/reducers/index';
 import { configureStore } from '@reduxjs/toolkit'
-import { legacy_createStore as createStore} from 'redux'
+import { legacy_createStore as createStore } from 'redux'
 // import { createStore } from 'redux';
 
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
