81dda002185c8a809c07941d13842cf58a322bb4
Maxim Bobyr <max89.bobyr@yandex.ru>
2023-10-24 01:10:40 +0300

    feat: add middleware

--- src/index.tsx
+++ src/index.tsx
@@ -10,12 +10,32 @@ import { rootReducer } from './services/reducers/index';
 import { configureStore } from '@reduxjs/toolkit'
 import { legacy_createStore as createStore } from 'redux'
 import { BrowserRouter as Router } from 'react-router-dom';
+import { socketMiddleware } from './services/middleware/wsSocket';
+import {
+  WS_SOCKET_OPEN, WS_SOCKET_OPEN_PRIVAT, WS_OPEN_SUCCESS, WS_SOCKET_CLOSE, WS_CLOSE_SUCCESS,
+  WS_SOCKET_ERROR, WS_SOCKET_ONMESSAGE, WS_SOCKET_ONMESSAGE_PRIVAT
+} from './services/actions/wsSocket';
+
+const wsActions = {
+  open: WS_SOCKET_OPEN,
+  close: WS_SOCKET_CLOSE,
+  onOpen: WS_OPEN_SUCCESS,
+  onClose: WS_CLOSE_SUCCESS,
+  error: WS_SOCKET_ERROR,
+  onMessage: WS_SOCKET_ONMESSAGE,
+  openPrivate: WS_SOCKET_OPEN_PRIVAT,
+  onMessagePrivate: WS_SOCKET_ONMESSAGE_PRIVAT
+}
+
+const wsUrl = 'wss://norma.nomoreparties.space/orders/all';
+const wsPrivateURL = 'wss://norma.nomoreparties.space/orders';
+
 
 // import { createStore } from 'redux';
 
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const enhancer = composeEnhancers(applyMiddleware(thunk));
+const enhancer = composeEnhancers(applyMiddleware(thunk, socketMiddleware(wsActions, wsUrl, wsPrivateURL)));
 
 // Инициализируем хранилище с помощью корневого редьюсера
 
